2311f4acb6da0b61aba9a2ec951af81bd9096119
dbsdk0070 <dbsdk0070@naver.com>
2021-03-30 17:28:02 +0900

    test

---
+++ 0.environment/src/kr/co/bitcamp/environment/Helloworld.java
@@ -0,0 +1,21 @@
+package kr.co.bitcamp.environment;
+
+public class Helloworld {
+    /*
+     * 자바 코드 작성 => 최종 실행되는 과정
+     * 
+     * [1] 자바 언어 (컴파일 언어)
+     *        자바 코드 작성(Helloworld.java)
+     *     -> 자바 코드 컴파일(Compile => Helloworld.class)
+     *     -> 자바 코드 실행(Run)
+     * 
+     * 
+     * [2] 파이썬 언어 (인터프리터 언어) - 컴파일 단계 없음
+     *        파이썬 코드를 작성(Helloworld.py)
+     *     -> 파이썬 코드 실행(Run)
+     * 
+     * 
+     */
+    
+
+}
